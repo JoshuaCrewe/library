80db03706e6af8733850e2a7d6964456435c7b27
Joshua Crewe <joshua@venncreative.co.uk>
2019-07-22 09:34:56 +0100

    Add a svelte front end to lumen API

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,7 @@
-import App from "./component.svelte";
+import App from "./App.svelte";
 
 const app = new App({
     target: document.body,
-    props: {
-        rounded: true
-    }
 });
 
 export default app;
--- resources/assets/js/component.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	let name = 'you';
-</script>
-
-<h1>Hello {name}!</h1>
