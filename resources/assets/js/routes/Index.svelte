63eb6cca1e8acec3ec3c9ffcf95de8be05aa2b66
Joshua Crewe <joshua@venncreative.co.uk>
2019-12-06 15:35:27 +0000

    Very quickly bash out the reskin

--- resources/assets/js/routes/Index.svelte
+++ resources/assets/js/routes/Index.svelte
@@ -2,6 +2,7 @@
     import {link} from 'svelte-spa-router'
 
     import Form from './../components/Form.svelte'
+    import Item from './../components/Item.svelte'
 
     import {items} from './../stores';
 
@@ -26,22 +27,7 @@
 {#if $items.results.length !== 0}
     <div class="items">
         {#each $items.results as item}
-            <a href="#/item/{item.id}" class="item">
-                <div class="item-image">
-                    <img src="{item.image}" alt="{item.title}">
-                </div>
-                <div class="item-details">
-                    <h2>
-                        {item.title}
-                    </h2>
-                    <p class="item-format">
-                        {item.format}
-                    </p>
-                    <p>
-                        {item.summary}
-                    </p>
-                </div>
-            </a>
+            <Item {item}/>
         {/each}
     </div>
 {/if}
@@ -53,47 +39,10 @@
     }
 
     .items {
-        margin: 0 auto;
+        margin: 0 auto 0rem;
         display: flex;
         flex-wrap: wrap;
-        max-width: 600px;
-    }
-
-    .item {
-        display: flex;
-        width : 100%;
-        flex: 1 1 100%;
-        padding: 1rem 0;
-    }
-
-    .item-image {
-        position: relative;
-        padding-bottom: 75%;
-        flex: 1;
-    }
-
-    .item-details {
-        flex: 1;
-        padding-left: 1rem;
-    }
-
-    .item-image img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        object-fit: cover;
-    }
-    
-    .item-format {
-        border: 1px solid green;
-        border-radius: 5px;
-        color: green;
-        display: inline-block;
-        padding: .25rem .5rem;
+        max-width: 75%;
     }
 
     .form-wrap {
@@ -116,5 +65,12 @@
 
     .subtitle {
         margin-bottom: 1.5rem;
+    }
+    @media (max-width: 900px) {
+        .items {
+            max-width: 100%;
+            padding: 0 2rem;
+        }
+
     }
 </style>
