efbbd634cb5b7b8c7965bccaea7b89f01612b52f
Joshua Crewe <me@joshuacrewe.co.uk>
2020-09-04 16:02:19 +0100

    Configure tailwind to see the templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [
+        "./resources/assets/js/**/*.svelte",
+        "./resources/assets/js/**/*.js",
+        "./resources/assets/views/**/*.php"
+    ],
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+};
